38a2876818e6d1e9c057d935f955b9180bcfa849
dongardivepravin1992 <dongardivepravin1992@gmail.com>
2023-09-22 08:30:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+    const createh1 = React.createElement('h1', {}, 'Hello World from React!');
+    const createp = React.createElement('p', {}, 'Hello World from React!');
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(createh1)
